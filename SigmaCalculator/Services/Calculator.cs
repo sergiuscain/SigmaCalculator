c67724d67c26bd1f05bc53069da1f72a6f7d9083
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-02-12 00:31:02 +0300

    Попытка реализовать нормальную логику вычисления калькулятора. В итоге решил просто использовать сторонние библиотеки, но там тоже проблемы с вычислением квадратного корня

--- SigmaCalculator/Services/Calculator.cs
+++ SigmaCalculator/Services/Calculator.cs
@@ -1,104 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flee.PublicTypes;
+using System;
+using static Android.Renderscripts.ScriptGroup;
 
 namespace SigmaCalculator.Services
 {
     public class Calculator
     {
-        public readonly Dictionary<string, int> precedence = new Dictionary<string, int>
+        public string EvaluateExpression(string expression)
         {
-            { "+", 1 },
-            { "-", 1 },
-            { "*", 2 },
-            { "/", 2 },
-            { "^", 3 },
-            { "sqrt", 4 }
-        };
-
-        public double Evaluate(string expression)
-        {
-            Stack<double> values = new Stack<double>();
-            Stack<string> operators = new Stack<string>();
-            int i = 0;
-
-            while (i < expression.Length)
+            var pattern = @"Sqrt\((.*?)\)";
+            var replacement = "$1^(1/2)";
+            try
             {
-                char c = expression[i];
+                // Проверка на деление на ноль
+                if (expression.Contains("/0"))
+                {
+                    return "Error";
+                }
 
-                if (char.IsDigit(c) || (c == '.' && i > 0 && char.IsDigit(expression[i - 1])))
-                {
-                    //Считаем число
-                    string num = "";
-                    while (i < expression.Length && char.IsDigit(expression[i]) || expression[i] == '.')
-                    {
-                        num += expression[i];
-                        i++;
-                    }
-                    values.Push(double.Parse(num));
-                    continue;
-                }
-                else if (precedence.ContainsKey(c.ToString()))
-                {
-                    while (operators.Count > 0 && precedence[operators.Peek()] >= precedence[c.ToString()])
-                    {
-                        ApplyOperator(operators, values);
-                    }
-                    operators.Push(c.ToString());
-                }
-                else if (c == '(')
-                {
-                    operators.Push(c.ToString());
-                }
-                else if (c == ')')
-                {
-                    while (operators.Count > 0 && operators.Peek() != "(")
-                    {
-                        ApplyOperator(operators, values);
-                    }
-                    operators.Pop();//Убираем "("
-                }
-                i++;
+                // Создаем контекст для вычисления выражения
+                var context = new ExpressionContext();
+
+                // Добавляем поддержку функции Sqrt
+                context.Variables["Sqrt"] = new Func<double, double>(Math.Sqrt);
+
+                //Заменяем Sqrt на результат вычисления квадратного корня
+                var newExpression = System.Text.RegularExpressions.Regex.Replace(expression, pattern, replacement);
+
+                // Вычисляем результат
+                var expr = context.CompileGeneric<double>(newExpression);
+                var result = expr.Evaluate();
+
+                return result.ToString(); // Преобразуем результат в строку
             }
-            while (operators.Count > 0)
+            catch (Exception)
             {
-                ApplyOperator(operators, values);
-            }
-            return values.Pop();
-        }
-
-        private void ApplyOperator(Stack<string> operators, Stack<double> values)
-        {
-            string op = operators.Pop();
-            if (op == "sqrt")
-            {
-                double value = values.Pop();
-                values.Push(Math.Sqrt(value));
-            }
-            else
-            {
-                double right = values.Pop();
-                double left = values.Pop();
-                switch (op)
-                {
-                    case "+":
-                        values.Push(left + right);
-                        break;
-                    case "-":
-                        values.Push(left - right);
-                        break;
-                    case "*":
-                        values.Push(left * right);
-                        break;
-                    case "/":
-                        values.Push(left / right);
-                        break;
-                    case "^":
-                        values.Push(Math.Pow(left, right));
-                        break;
-                }
+                return "Error"; // Общая обработка ошибок
             }
         }
     }
--- SigmaCalculator/ViewModel/MainViewModel.cs
+++ SigmaCalculator/ViewModel/MainViewModel.cs
@@ -1,9 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SigmaCalculator.Services;
 
 namespace SigmaCalculator.ViewModel
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
-        
+        private readonly Calculator calculator;
+        public MainViewModel(Calculator calculator) 
+        {
+            this.calculator = calculator;
+        }
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Calculate()
+        {
+            var calculateResult = calculator.EvaluateExpression(text);
+            Text = calculateResult;
+        }
+
+        [RelayCommand]
+        void Add(string s)
+        {
+            Text += s;
+        }
+
+        [RelayCommand]
+        void AS(string s)
+        {
+            Text = "";
+        }
+
     }
 }
